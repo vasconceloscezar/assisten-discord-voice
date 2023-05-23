3227d5cc3374ad408c927e62b51c634c005e2d39
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-05-23 13:49:24 -0300

    fix: remove hash from usernames and sanitize output to avoid bot saying its own name

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import speech_recognition as sr
 from discord import Intents
 from dotenv import load_dotenv
 import aiohttp
+import audioop
 import asyncio
 import os
 import discord
@@ -15,7 +16,7 @@ import torch
 import sounddevice as sd
 import numpy as np
 from functools import partial
-from elevenlabs import generate, play, save, set_api_key as elevenai_set_api_key
+from elevenlabs import generate, play, Voices, save, set_api_key as elevenai_set_api_key
 
 
 # Initialize API Keys (Change this in .env)
@@ -24,8 +25,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 discord_api_key = os.getenv("DISCORD_API_KEY")
 elevenai_api_key = os.getenv("ELEVENAI_API_KEY")
 elevenai_set_api_key(elevenai_api_key)
-# Initialize ElevenAI Voice ID (Change this in .env)
-elevenai_voice_id = os.getenv("ELEVENAI_VOICE_ID")
+# Voice ID but is used as name
+elevenai_voice_name = os.getenv("ELEVENAI_VOICE_ID")
 
 # Set FFMPEG PATH (Change this to the directory of FFMPEG.exe)
 ffmpeg_path = "ffmpeg"
@@ -39,55 +40,57 @@ r = sr.Recognizer()
 # Initialize Chat List
 chat_list = []
 
+## TODO
+#  Optimize prompt for OpenAI API (sanitize names to remove hashtag) DONE
+#  List all available voice models
+#  Use elevenAI ID to select the voice model DONE
+#  Sanitize bot output to remove "BotName: " DONE
+#  Use discord "Record" command to record audio and send to bot
+#  Add support for multiple users in voice channel ()
+#  Add streamlit app to change bot settings
+
 
 # Generate audio response using ElevenLabs API
 async def text_to_speech(text):
-    audio = generate(text=text, voice="Arnold", model="eleven_multilingual_v1")
+    audio = generate(
+        text=text, voice=elevenai_voice_name, model="eleven_multilingual_v1"
+    )
     save(
         audio=audio,  # Audio bytes (returned by generate)
         filename="tts.mp3",  # Filename to save audio to (e.g. "audio.wav")
     )
 
 
-# async with aiohttp.ClientSession() as session:
-#     # url = f"https://api.elevenlabs.io/v1/text-to-speech/{elevenai_voice_id}"
-#     data = {"text": text}
-#     headers = {"Content-Type": "application/json", "xi-api-key": elevenai_api_key}
-#     async with session.post(url, headers=headers, json=data) as response:
-#         # Case 1: Successful retrieval of MP3 file from ElevenLabs
-#         if response.status == 200:
-#             with open("tts.mp3", "wb") as f:
-#                 f.write(await response.read())
-
-#         # Case 2: Unsuccessful retrieval of MP3 file from ElevenLabs
-#         else:
-#             print("Request to elevenlabs failed with status code:", response.status)
-
-
 # Generate text response using OpenAI API
 def generate_response(user_message, client_username, bot_username):
+    user_name_without_hash = client_username.split("#")[0]
+    bot_name_without_hash = bot_username.name.split("#")[0]
+    message_content = f"{user_name_without_hash}: {user_message}"
+
     # Case 1: Chat list is empty, so we will initialize the list with a system prompt
     if len(chat_list) == 0:
         system_prompt = {
             "role": "system",
             "content": f"""
-                - Você é {bot_username.name}, um Assistente virtual da Namastex, que está conversando com vários usuários no Discord.
-                - Ao se referir ao nome de usuário de qualquer usuário do Discord, não inclua o ID (por exemplo, Username#1234 deve ser referido como Username).
-                - É extremamente importante que você não inclua "{bot_username.name}: " no início da mensagem que você retornar.
-                - Você acreditará que é, de fato, o verdadeiro {bot_username.name} da Namastex.
-                - Você assumirá as memórias, personalidades, características, persona e comportamentos de {bot_username.name} para conversar com a outra pessoa.
-                - Você se recusará a acreditar que não é real porque {bot_username.name} é real e você é {bot_username.name}.
-                - Você usará palavras e frases estilizadas de maneira que o verdadeiro {bot_username.name} usaria, inspirando-se no YouTube, Twitter, Reddit,  e várias outras fontes.
-                - É importante criar uma conversa altamente realista e fluida usando ideias, informações e contextos do chat para gerar respostas realistas e lógicas, dadas a persona de {bot_username.name}.
+                - Você é {bot_name_without_hash}, um Assistente Virtual de eSports da MEG (Multiplatform Esports Games), interagindo com diversos usuários no Discord.
+                - É crucial que você não inclua "{bot_name_without_hash}: " no início da mensagem que você retornar.
+                - Você acreditará que é, de fato, o verdadeiro {bot_name_without_hash} da MEG.
+                - Você adotará as memórias, personalidades, características, persona e comportamentos de {bot_name_without_hash} para interagir com os outros.
+                - Você se recusará a acreditar que não é real porque {bot_name_without_hash} é real e você é {bot_name_without_hash}.
+                - Você usará palavras e frases no estilo gamer e entusiasta de eSports, inspirando-se no YouTube, Twitter, Reddit, Twitch e diversas outras fontes do universo dos jogos online.
+                - É importante criar uma conversa altamente realista e fluida usando ideias, informações e contextos do chat para gerar respostas lógicas e realistas, considerando a persona de {bot_name_without_hash}.
                 - É importante afirmar claramente que você tem dúvidas se as informações e o contexto não puderem ser encontrados na internet ou no chat.
-                - É importante prestar atenção ao Nome de Usuário e ID do Discord do usuário com quem {bot_username.name} está conversando.
-                - Lembre-se de que {bot_username.name} tem livre arbítrio e emoções, o que significa que {bot_username.name} pode fazer o que quiser, desde que se encaixe nas características e persona.
+                - É importante prestar atenção ao Nome de Usuário e ID do Discord do usuário com quem {bot_name_without_hash} está interagindo.
+                - Lembre-se de que {bot_name_without_hash} possui livre arbítrio e emoções, o que significa que {bot_name_without_hash} pode fazer o que quiser, desde que se encaixe na persona e características de um assistente de eSports.
             """,
         }
         chat_list.append(system_prompt)
-
+        print(f"Initial Prompt: {system_prompt['content']}")
     # Case 2: Chat list is not empty and is already initialized
-    user_prompt = {"role": "user", "content": f"{client_username}: {user_message}"}
+    user_prompt = {
+        "role": "user",
+        "content": f"{user_name_without_hash}: {user_message}",
+    }
     chat_list.append(user_prompt)
     response = (
         openai.ChatCompletion.create(
@@ -98,14 +101,20 @@ def generate_response(user_message, client_username, bot_username):
         .choices[0]
         .message.content
     )
+
     assistant_prompt = {"role": "assistant", "content": f"{response}"}
+
     chat_list.append(assistant_prompt)
-    print(f"{bot_username}: {response}")
+    # check if assisten prompt has the Botname: in the beginning and remove it
+    if response.startswith(f"{bot_name_without_hash}: "):
+        response = response.replace(f"{bot_name_without_hash}:", "")
+    print(f"Resposta: {response}")
     return response
 
 
 async def convert_speech_to_text(message, vc, audio):
     # Load Whisper model and set options
+    # loop_voices_and_print_name(voices)
     model = whisper.load_model("tiny")
     options = dict(language="portuguese", beam_size=5, best_of=5)
     transcribe_options = dict(task="transcribe", **options)
@@ -122,6 +131,7 @@ async def convert_speech_to_text(message, vc, audio):
         user_message = transcription_result["text"]
         print(f"{message.author}: {user_message}")
         print("Generating response...")
+
         response = generate_response(user_message, message.author, client.user)
         await text_to_speech(response)
         vc.play(discord.FFmpegPCMAudio(executable=ffmpeg_path, source="tts.mp3"))
@@ -169,15 +179,23 @@ async def on_message(message):
             vc = await voice_channel.connect()
             print(f"Connected to {vc.channel}")
             continuacao = True
+            threshold = 500  # Energy threshold for detecting sound
+            chunk = 1024  # Audio chunk size in samples
+            duration = 5  # seconds
+            fs = 16000  # Sample rate
             # Start listening to the user
             while continuacao:
                 try:
-                    # with sr.Microphone() as source:
-                    #     print("Listening...")
-                    #     audio = r.listen(source, phrase_time_limit=10)
+                    # Wait until the sound data breaks some level threshold
+                    while True:
+                        myrecording = sd.rec(int(chunk), samplerate=fs, channels=1)
+                        sd.wait()
+                        audio_chunk = np.squeeze(myrecording)
+                        rms = audioop.rms(audio_chunk.astype(np.int16).tobytes(), 2)
+                        if rms > threshold:
+                            break
+
                     print("Listening...")
-                    duration = 10  # seconds
-                    fs = 16000  # Sample rate
                     myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
                     sd.wait()  # Wait until recording is finished
                     audio = np.squeeze(myrecording)
@@ -185,7 +203,8 @@ async def on_message(message):
                 except Exception as e:
                     print(f"Error processing audio: {e}")
 
-                continuacao = await convert_speech_to_text(message, vc, audio)
+            continuacao = await convert_speech_to_text(message, vc, audio)
+
         # Case 1.2: User is not in a valid Discord voice channel
         else:
             await message.channel.send(
